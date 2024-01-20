7219e148d850772d6348c96b0ae3c0e74ebd2c28
sticktostack <rajdeep6153@gmail.com>
2024-01-21 02:53:00 +0530

    first commit

---
+++ Responsive/script.js
@@ -0,0 +1,46 @@
+gsap.from("#one",{
+    delay:0.5,
+    duration:1,
+    y:-40,
+    opacity:0
+})
+gsap.from("#two",{
+    delay:0.5,
+    duration:1,
+    y:40,
+    opacity:0
+})
+gsap.from("#three",{
+    delay:0.5,
+    duration:1,
+    y:-40,
+    opacity:0
+})
+gsap.from("a",{
+    x:60,
+    delay:0.5,
+    duration:1,
+    opacity:0
+})
+gsap.from(".nav h1",{
+    x:-60,
+    delay:0.5,
+    duration:1,
+    opacity:0
+})
+gsap.from(".content h1",{
+    // y:-60,
+    delay:0.5,
+    duration:4,
+    opacity:0
+})
+gsap.from(".content h3",{
+    delay:0.5,
+    duration:2,
+    opacity:0
+})
+gsap.from(".content i",{
+    delay:0.5,
+    duration:2,
+    opacity:0
+})
